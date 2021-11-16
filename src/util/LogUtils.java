6d454d16c6611a516a0a629cdb3d42b1ca7a65f0
wy <wyforme88@gmail.com>
2021-11-16 17:32:04 +0800

    修改日志打印

--- src/util/LogUtils.java
+++ src/util/LogUtils.java
@@ -7,25 +7,8 @@ import java.util.logging.Logger;
 public class LogUtils {
 
     private static LogUtils singleton = null;
-
     public Logger sysException;
-
     public int i = 0;
-
-    private LogUtils() {
-        sysException = null;
-        //使用相对路径
-        String path = (LogUtils.class.getClassLoader().getResource("")).toString();
-        System.out.println(path);
-
-        //去掉  file:/
-        path = path.substring(6);
-        System.out.println(path);
-
-        path = path + "log4j.properties";
-        PropertyConfigurator.configure(path);
-        sysException = Logger.getLogger("sysException");
-    }
 
     public static LogUtils getInstance() {
         try {
@@ -35,6 +18,21 @@ public class LogUtils {
         }
         return singleton;
     }
+
+    private LogUtils() {
+        sysException = null;
+        //使用相对路径
+        String path = (LogUtils.class.getClassLoader().getResource("")).toString();
+        System.out.println(path);
+
+        path = path.substring(6);
+        System.out.println(path);
+
+        path = path + "log4j.properties";
+        PropertyConfigurator.configure(path);
+        sysException = Logger.getLogger("sysException");
+    }
+
 
     static void init() throws Exception {
         singleton = new LogUtils();
